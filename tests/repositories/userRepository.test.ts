8b44b7f9e8973962a6360dfee784661b104c5c0c
Kaua Onorato <kauaonorato.dev@gmail.com>
2025-01-08 22:09:48 -0400

    feat: Testando metodo que retorna o usuario pelo seu ID do userRepository

--- tests/repositories/userRepository.test.ts
+++ tests/repositories/userRepository.test.ts
@@ -5,23 +5,44 @@ import LocalUserRepository from "../../src/repositories/LocalUserRepository";
 describe('Repository methods Test', () => {
     let userRepository: IUserRepository;
 
+    const dataUserTest = {
+        name: 'test',
+        email: 'test@test.com'
+    }
+
     function isDefinedName(usersList: User[], name: string): boolean {
-        if (usersList[0].name !== name) {
+        const user = usersList.some(user => user.name === name);
+        
+        if (!user) {
             throw new Error('Name not defined');
         }
 
-        return true;
+        return user;
     }
+
+    function returnUserId(userslist: User[], userName: string): string {
+        const filteredUser = userslist.find(user => user.name === userName);
+
+        if (!filteredUser) {
+            throw new Error('User not found');
+        }
+
+        const userId = filteredUser.id;
+        return userId;
+    } 
 
     beforeAll(() => {
         userRepository = new LocalUserRepository();
+
+        userRepository.createUser({
+            name: 'name_test',
+            email: 'emailTest@email.com'
+        });
     });
 
+
     it('createUser method must create the user correctly', async () => {
-        userRepository.createUser({
-            name: 'test',
-            email: 'test@test.com'
-        });
+        userRepository.createUser(dataUserTest);
 
         const users = await userRepository.getUsers();
         const definedName = isDefinedName(users, 'test');
@@ -29,5 +50,21 @@ describe('Repository methods Test', () => {
         expect(users).toBeDefined();
         expect(definedName).toBeTruthy();
         
+    });
+
+
+    it('createUser method should not create an existing user', async () => {
+        userRepository.createUser()
+    });
+
+
+    it('getUserById method must return the user correctly', async () => {
+        const usersList = await userRepository.getUsers();
+        const userId = returnUserId(usersList, 'name_test')
+
+        const user = await userRepository.getUserById(userId);
+
+        expect(user).toBeDefined();
+        expect(user.name).toBe('name_test');
     });
 });
